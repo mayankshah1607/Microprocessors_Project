de157d8460a2b9d1505a0b908f1cace00e8bd2fa
mayankshah1607 <mayankshah1614@gmail.com>
2019-03-21 20:07:29 +0530

    Updates

---
+++ precache-manifest.ea26fe6a25818c6db1cb3a1b6e45d12e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2967f4fdfed39e741d",
+    "url": "/Microprocessors_Project/static/js/runtime~main.65c5ae4b.js"
+  },
+  {
+    "revision": "6e967c83299e99e1a1c7",
+    "url": "/Microprocessors_Project/static/js/main.c876f717.chunk.js"
+  },
+  {
+    "revision": "2c10bab96cc7b40083f9",
+    "url": "/Microprocessors_Project/static/js/2.1370c159.chunk.js"
+  },
+  {
+    "revision": "6e967c83299e99e1a1c7",
+    "url": "/Microprocessors_Project/static/css/main.69a7c67f.chunk.css"
+  },
+  {
+    "revision": "b08c21913fcab24a876f99ff7551b37d",
+    "url": "/Microprocessors_Project/index.html"
+  }
+];
